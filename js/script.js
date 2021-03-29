f7b350b06538b34d6923db99259e72cabe004c80
Eduard <example@gmail.com>
2021-03-30 01:15:04 +0300

    Первое задание

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 "use strict";
-const result = confirm('Are you here?');
-console.log(result);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
+let personalMovieDB={
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const film1 = prompt('Один из последних просмотренных фильмов?', '');
+const rating1 = prompt('На сколько оцените его?', '');
+const film2 = prompt('Один из последних просмотренных фильмов?', '');
+const rating2 = prompt('На сколько оцените его?', '');
+personalMovieDB.movies[film1] = rating1;
+personalMovieDB.movies[film2] = rating2;
+
 
 
